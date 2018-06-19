7c79ecaf5ded64643028f28911673cc0755ad005
travers-rhodes <traversr@andrew.cmu.edu>
2018-06-19 11:39:30 -0400

    add launch file, but logs not showing up

--- niryo_one_tutorial/src/move_to_goal.cpp
+++ niryo_one_tutorial/src/move_to_goal.cpp
@@ -1,6 +1,22 @@
-#include <moveit/move_group_interface/move_group.h>
-
+#include <moveit/robot_model_loader/robot_model_loader.h>
+#include <moveit/robot_model/robot_model.h>
+#include <moveit/robot_state/robot_state.h>
+#include <ros/console.h>
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "move_to_goal");
+  if( ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug) ) {
+     ros::console::notifyLoggerLevelsChanged();
+  }
+  robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
+  robot_model::RobotModelPtr kinematic_model = robot_model_loader.getModel();
+  ROS_INFO("Model frame");
+  ros::Duration(2).sleep();
+  ROS_INFO("Model frame: %s", kinematic_model->getModelFrame().c_str());
+  ros::Duration(2).sleep();
+  ROS_INFO("Model frame");
+
+  robot_state::RobotStatePtr kinematic_state(new robot_state::RobotState(kinematic_model));
+  kinematic_state->setToDefaultValues();
+  const robot_state::JointModelGroup* joint_model_group = kinematic_model->getJointModelGroup("arm");
 }
